c5719f28d8c9cb0d66dfd336bd3f7599fee5176c
P86071244 <P86071244@gs.ncku.edu.tw>
2019-09-19 00:23:31 +0800

    改善精準度.

--- Course/L1/demo2.c
+++ Course/L1/demo2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
     float sum = 0.0f, corr = 0.0f; /* corrective value for rounding error */
-    for (int i = 0; i < 10000 * 3; i++) {
+    for (int i = 0; i < 10000; i++) {
         float y = (i + 1) - corr; /* add the correction to specific item */
         float t = sum + y; /* bits might be lost */
         corr = (t - sum) - y; /* recover lost bits */
