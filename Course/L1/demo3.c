710ebb3a6b519a6b40c7e4007d63c1d6cb68c78b
P86071244 <P86071244@gs.ncku.edu.tw>
2019-09-19 10:21:13 +0800

    PPT_replace the last value_will get different result.

--- Course/L1/demo3.c
+++ Course/L1/demo3.c
@@ -1,4 +1,5 @@
 //PPT_replace the last value_will get different result.
+/*
 #include <stdint.h>
 #include <stdio.h>
 int32_t abs(int32_t x) {
@@ -7,4 +8,16 @@ int32_t abs(int32_t x) {
 }
 int main() {
 	printf("=> %d\n", abs(-2147483648));
+}
+*/
+#include <stdio.h>
+#include <stdint.h>
+int32_t test(int32_t x) {
+    int mask = x >> 31;
+    printf("mask = %d, x + mask = %d\n", mask, x + mask);
+    return (x + mask) ^ mask;
+}
+int main(void) {
+    printf("abs(INT32_MIN) = %d\n", test(-2147483648));
+    return 0;
 }
