5026fe86a285c977604d407ac567afb8a1ddb60e
P86071244 <P86071244@gs.ncku.edu.tw>
2019-09-19 13:11:18 +0800

    struct size.

--- Course/L1/demo15.c
+++ Course/L1/demo15.c
@@ -4,9 +4,22 @@
 typedef struct _s1 {
     char a[5];
 } s1;
+//#pragma pack(1)
+typedef struct _test1 {
+    char c[3];
+    int num[256];
+} test1;
+//#pragma pack(pop)
+
+typedef struct _test2 {
+    char c[3];
+    int num[256];
+} test2;
 int main() {
     s1 p[10];
     printf("struct s1 size: %ld byte\n", sizeof(s1));
+	printf("struct s1 size: %ld byte\n", sizeof(test1));
+	printf("struct s1 size: %ld byte\n", sizeof(test2));
     for(int i = 0; i < 10; i++) {
         printf("the struct p[%d] address =%p\n", i, p + i);
     }
