474af0b1a4927253c6b4cf1efc3e31a9c2b0629c
P86071244 <P86071244@gs.ncku.edu.tw>
2019-09-25 05:02:48 +0800

    Quiz1. Lab.

--- Course/L1/demo6.c
+++ Course/L1/demo6.c
@@ -12,7 +12,20 @@ int main(){
 	printf("%08x\n", 0x1997 + (4-1) & -4);
 	printf("%08x\n", 0x1997 + 3);
 	int p = 0x1997;
+	printf("%d\n", p);
 	printf("%x\n", p);
+	printf("%016x\n", p);
 	printf("%08x\n", align4(p, 3));
+	printf("%d\n", ~~~0x03);
+	printf("%08x\n", -4);
+	printf("%08x\n", (((0x1006) + (0x03)) & (~0x03)));
+	__uint32_t aa = 1;
+	printf("value of a = 0x%x\n", aa);
+	int arr[5] = {0};
+	printf("%d\n", arr[4]);
+	int mask = -123 >> 1;
+	printf("%d\n", mask);
+	int pp = 0x01;
+	printf("%x\n", pp);
 	return 0;
 }
