812ffaf22b12c53415172ebb98b7d1f0ce7b6f73
P86071244 <P86071244@gs.ncku.edu.tw>
2019-09-25 05:12:47 +0800

    Quiz1. Lab.

---
+++ Course/L1/demo16.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+#define ALIGN(x,a)        __ALIGN_MASK(x,(typeof(x))(a)-1)
+#define __ALIGN_MASK(x,mask)	(((x)+(mask))&~(mask))
+
+int main() {
+	int x = 0x1006;
+	printf("%08x\n", ALIGN(x, 4));
+	return 0;
+}
